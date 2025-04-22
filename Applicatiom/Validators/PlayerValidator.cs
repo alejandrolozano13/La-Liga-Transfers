472a3024b11691b7d5a810f6f565bde60c8e1027
alelk <alejandro20181999@gmail.com>
2025-04-22 13:56:15 -0300

    finalizei o fluent validation do player

--- Applicatiom/Validators/PlayerValidator.cs
+++ Applicatiom/Validators/PlayerValidator.cs
@@ -7,15 +7,32 @@ namespace Applicatiom.Validators
     {
         public PlayerValidator()
         {
-            // por default deixamos as regras de validacao aqui no construtor.
-            
             RuleFor(player => player.Name)
                 .NotEmpty()
                 .WithMessage("O nome é obrigatório.")
                 .Length(2, 150)
                 .WithMessage("O nome deve possuir entre 2 e 150 caracteres");
 
+            RuleFor(player => player.BirthdayDate)
+                .NotEmpty()
+                .WithMessage("A data de nascimento é obrigatória")
+                .Must(BeValidAge)
+                .WithMessage("O jogador deve ter entre 16 e 50 anos");
 
+            RuleFor(player => player.ClubId)
+                .NotEmpty()
+                .WithMessage("O clube de jogador é obrigatório");
+
+            RuleFor(player => player.Position)
+                .IsInEnum()
+                .WithMessage("A posição do jogador é inválida.");
+        }
+
+        private bool BeValidAge(DateTime date)
+        {
+            var age = DateTime.Today.Year - date.Year;
+            if (date.Date > DateTime.Today.AddYears(-age)) age--;
+            return age >= 16 && age <= 50;
         }
     }
 }
